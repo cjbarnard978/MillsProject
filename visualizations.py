3e3a33bdd4dd62834cbc0c2065e2937ff06db43d
Cecilia Barnard <barnar3@clemson.edu>
2026-04-06 16:26:46 -0400

    created denomination pie chart

--- visualizations.py
+++ visualizations.py
@@ -3,4 +3,6 @@ import plotly.express as px
 df = pd.read_csv('CourtenayMillsRecords.csv')
 fig = px.pie(df, values='ID Number', names='Denomination')
 fig.show()
+fig.write_image('denominationbyidnumber.png')
+
 
