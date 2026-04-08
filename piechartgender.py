82a78753ee457caf2f1018c22cd9c32c37b25b8a
Cecilia Barnard <barnar3@clemson.edu>
2026-04-08 18:36:58 -0400

    added query to gendered pie chart

--- piechartgender.py
+++ piechartgender.py
@@ -1,3 +1,6 @@
 import pandas as pd 
 import plotly.express as px
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("M/F == 'F'")
+df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Gender == 'F'")
+fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Women')
+fig.show()
+
