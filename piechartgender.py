e2a61d38802152d6632809df546b5660ec737124
Cecilia Barnard <barnar3@clemson.edu>
2026-04-08 18:43:42 -0400

    created pie chart for marital status of respondents

--- piechartgender.py
+++ piechartgender.py
@@ -4,3 +4,10 @@ df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Gender == 'F'")
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Women')
 fig.show()
 
+df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Gender == 'M'")
+fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Men')
+fig.show()
+
+df = pd.read_csv('CourtenayMillsRecordsEdited.csv')
+fig = px.pie(df, values='ID Number', names='Marital Status', title='Marital Status of Employees who Identified a Religious Affiliation')
+fig.show()
