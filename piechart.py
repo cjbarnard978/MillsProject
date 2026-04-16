27cd5fdb3563b29b4979f02d551b0520cffc7316
Cecilia Barnard <barnar3@clemson.edu>
2026-04-16 11:06:04 -0400

    corrected CSV, added new CSV name to all pd.read_csv slots

--- piechart.py
+++ piechart.py
@@ -4,7 +4,3 @@ df = pd.read_csv('CourtenayMillsRecordsEdited.csv')
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Mill Employees')
 fig.show()
 
-count = df['ID Number'].value_counts()
-df = df.assign('count')
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Denomination != 'Baptist'")
-df = px.bar(df, x='count', y='Denomination', title='Religious Affiliations of Employees who are Not Baptist')
--- piechartgender.py
+++ piechartgender.py
@@ -1,14 +1,14 @@
 import pandas as pd 
 import plotly.express as px
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Gender == 'F'")
+df = pd.read_csv('CourtenayMillsRecordsCorrected.csv').query("Gender == 'F'")
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Women')
 fig.show()
 
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Gender == 'M'")
+df = pd.read_csv('CourtenayMillsRecordsCorrected.csv').query("Gender == 'M'")
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Men')
 fig.show()
 
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv')
+df = pd.read_csv('CourtenayMillsRecordsCorrected.csv')
 fig = px.pie(df, values='ID Number', names='Marital Status', title='Marital Status of Employees who Identified a Religious Affiliation')
 fig.show()
 
