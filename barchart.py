f5f835e6b486ca1d0af46d29aa9449ace41f5e46
Cecilia Barnard <barnar3@clemson.edu>
2026-04-16 11:09:59 -0400

    corrected CSV to include variable gender, re-ran all code, saved corrected pngs

--- barchart.py
+++ barchart.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv').query("Denomination != 'Baptist'")
+df = pd.read_csv('CourtenayMillsRecordsCorrected.csv').query("Denomination != 'Baptist'")
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Employees who are Not Baptist')
 fig.show()
--- piechart.py
+++ piechart.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import plotly.express as px
-df = pd.read_csv('CourtenayMillsRecordsEdited.csv')
+df = pd.read_csv('CourtenayMillsRecordsCorrected.csv')
 fig = px.pie(df, values='ID Number', names='Denomination', title='Religious Affiliations of Mill Employees')
 fig.show()
 
